f2eb49de7816849965348b812408bb8519e85e5e
Harold Pulcher <pulcher@pulcher.biz>
2022-09-19 22:35:28 -0500

    Initial load in of the briefcase software

---
+++ src/blu-clear-briefcase/blu-clear-briefcase.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+ * Stuff that needs defining in the blu-clear-briefcase
+ */
+
+#define BCB_READPACKET_TIMEOUT          10   // Timeout in ms waiting to read a response
+
+// Min and Max brightness ranges
+#define BCB_MIN_BRIGHT                  20
+#define BCB_MAX_BRIGHT                  130
+
+#define BCB_ANIMATION_CYCLES            10   // number of animation cycles to execute before exiting
+
+// Setup some defaults
+#define BCB_DEFAULT_COLOR_1             0xFFFFFF    // White
+#define BCB_DEFAULT_COLOR_2             0x0000FF    // Blue
